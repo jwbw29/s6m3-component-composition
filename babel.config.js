8f0154f06f8b65de198db8492398080d59ce45ed
Justin Byrd <justinbyrd7@gmail.com>
2024-02-06 14:52:56 -0600

    Update babel and styled-components configurations

--- babel.config.js
+++ babel.config.js
@@ -1,37 +1,25 @@
-const PLUGIN_TRANSFORM_RUNTIME = '@babel/plugin-transform-runtime'
-const PLUGIN_STYLED_COMPONENTS = 'babel-plugin-styled-components'
+const PLUGIN_TRANSFORM_RUNTIME = "@next/babel/plugin-transform-runtime";
+const PLUGIN_STYLED_COMPONENTS = "babel-plugin-styled-components";
 
-const PRESET_REACT = '@babel/preset-react'
-const PRESET_ENV = '@babel/preset-env'
+const PRESET_REACT = "@next/babel/preset-react";
+const PRESET_ENV = "@next/babel/preset-env";
 
 module.exports = {
   env: {
     testing: {
-      plugins: [
-        [PLUGIN_TRANSFORM_RUNTIME],
-      ],
+      plugins: [[PLUGIN_TRANSFORM_RUNTIME]],
       presets: [
         [PRESET_REACT],
-        [PRESET_ENV, { modules: 'commonjs', debug: false }]
-      ]
+        [PRESET_ENV, { modules: "commonjs", debug: false }],
+      ],
     },
     development: {
-      plugins: [
-        [PLUGIN_STYLED_COMPONENTS],
-      ],
-      presets: [
-        [PRESET_REACT],
-        [PRESET_ENV, { targets: { chrome: '110' } }]
-      ]
+      plugins: [[PLUGIN_STYLED_COMPONENTS]],
+      presets: [[PRESET_REACT], [PRESET_ENV, { targets: { chrome: "110" } }]],
     },
     production: {
-      plugins: [
-        [PLUGIN_STYLED_COMPONENTS],
-      ],
-      presets: [
-        [PRESET_REACT],
-        [PRESET_ENV, { targets: { chrome: '110' } }]
-      ]
+      plugins: [[PLUGIN_STYLED_COMPONENTS]],
+      presets: [[PRESET_REACT], [PRESET_ENV, { targets: { chrome: "110" } }]],
     },
-  }
-}
+  },
+};
